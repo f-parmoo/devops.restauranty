b2b9bf51590cc0de37d6f269f34d8de40b38237d
fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2026-05-27 13:21:44 +0200

    fix format

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,29 +1,29 @@
-const js = require('@eslint/js');
+const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'module',
+      sourceType: "module",
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
       globals: {
-        window: 'readonly',
-        document: 'readonly',
-        localStorage: 'readonly',
-        fetch: 'readonly',
-        console: 'readonly',
-        jest: 'readonly',
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        fetch: "readonly",
+        console: "readonly",
+        jest: "readonly",
       },
     },
     rules: {
-      'no-unused-vars': 'off',
-      'no-undef': 'off',
+      "no-unused-vars": "off",
+      "no-undef": "off",
     },
   },
 ];
