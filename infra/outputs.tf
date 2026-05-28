c0dbb409b1882f4836c6640d288a80ec28722674
fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2026-05-28 10:29:49 +0200

    feat: use ACR instead of dockerhub

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -25,3 +25,7 @@ output "production_url" {
 output "ingress_public_ip" {
   value = azurerm_public_ip.ingress.ip_address
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
